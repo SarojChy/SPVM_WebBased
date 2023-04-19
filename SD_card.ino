196e71f5c7c4f30c13e1dca5e37ba5c41cc6cbab
Saroj Chaudhary <sarojthaaru@gmail.com>
2023-04-19 18:50:38 +0545

    new tag adding and if same tag then increase tag count

--- SD_card.ino
+++ SD_card.ino
@@ -1,5 +1,104 @@
+void Update_Log(String tagUID) {
+  Serial.println("Received tag is : " + tagUID);
+  
 
-String readLog() {
+  dataFile = SD.open(logUID_Path, FILE_READ);
+  int c = 0;
+  if (!dataFile) {
+    Serial.println("Could not open file!");
+    return;
+  }
+  boolean tagExists = false;
+  while (dataFile.available()) {
+    String line = "";
+    line = dataFile.readStringUntil('\n');
+    int pos = line.indexOf(":");
+    String fileTag = line.substring(0, pos);
+    int count = line.substring(pos + 1).toInt();
+    if (fileTag.startsWith(tagUID)) {
+      tagExists = true;
+      Serial.println(fileTag + " ->Exists");
+      Serial.println("line :" + String(c) + "," + line);
+      update_Count(fileTag, count, c);
+      break;
+    }
+    c++;
+  }
+  dataFile.close();
+  if (!tagExists) {
+
+    dataFile = SD.open(logUID_Path, FILE_APPEND);
+    if (!dataFile) {
+      Serial.println("Could not open file!");
+      return;
+    }
+    String New_1 = each_uid + ":" + "1";
+    Serial.println("New Log: " + New_1);
+    dataFile.println(New_1);
+    dataFile.close();
+  }
+}
+
+
+void update_Count(String fileTag, int count, int c) {
+  dataFile.close();
+  char* array [500];
+  int line_count = 0;
+  count++;
+  String updated_tag = fileTag + ":" + String(count);
+  Serial.println(updated_tag);
+
+  dataFile = SD.open(logUID_Path, FILE_READ);
+  if (!dataFile) {
+    Serial.println("Could not open file!");
+    return;
+  }
+
+  // Allocate memory for each pointer in the array
+  for (int i = 0; i < sizeof(array) / sizeof(char*); i++) {
+    array[i] = (char*)malloc(15); // Allocate 15 bytes for each string
+  }
+
+  while (dataFile.available()) {
+
+    String line = "";
+    line = dataFile.readStringUntil('\n');
+
+    array[line_count] = (char*)malloc(line.length() + 1);
+
+    if (line_count == c) {
+      line = "";
+      line = updated_tag;
+    }
+    strcpy(array[line_count], line.c_str());
+    line_count++;
+  }
+  dataFile.close();
+
+  dataFile = SD.open(logUID_Path, FILE_WRITE);
+  if (!dataFile) {
+    Serial.println("Could not open file!");
+    return;
+  }
+  for (int i = 0; i < line_count; i++) {
+    Serial.println(array[i]);
+    //    Serial.println(array[i].length());
+    String new_line_data = String(array[i]);
+    new_line_data.trim();
+    dataFile.print(new_line_data);
+    dataFile.println();
+  }
+  dataFile.close();
+
+  //// Free the memory for each pointer in the array
+  for (int i = 0; i < sizeof(array) / sizeof(char*); i++) {
+    free(array[i]);
+  }
+}
+
+
+
+String postLog() {
   // Open the file for reading
   File file = SD.open(logUID_Path, FILE_READ);
   if (!file) {
